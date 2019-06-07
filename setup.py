7dce10ecdd01c19544929f04b3150abd4921bef1
Ali Abdelmalek <Ali@Alis-MBP.fios-router.home>
2019-06-07 19:20:57 -0400

    Version 1.0.5

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="youtubedata",
-    version="1.0.4",
+    version="1.0.5",
     author="ofc",
     author_email="support@youtubedata.io",
-    description="YouTube Data is a library that provides comprehensive scraping video metadata scraping. Results are returned in a dictionary and include likes, dislikes, views, publish dates and more.",
+    description="YouTube Data provides comprehensive YouTube video metadata scraping. Results are returned in a dictionary which contains likes, dislikes, views, published dates and more.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://www.youtubedata.io",
